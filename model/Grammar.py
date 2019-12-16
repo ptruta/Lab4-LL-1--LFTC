6e7e0f45c009825947f4c5b632643f10544270e5
truta <tpie2451@scs.ubbcluj.ro>
2019-12-16 23:31:04 +0200

    added grammar

---
+++ model/Grammar.py
@@ -0,0 +1,81 @@
+class Grammar:
+
+    def __init__(self, N, E, P, S):
+        self.N = N
+        self.E = E
+        self.P = P
+        self.S = S
+        self.read_from_file()
+
+    def is_non_terminal(self, value):
+        if value in self.N:
+            return True
+        return False
+
+    def is_terminal(self, value):
+        if value in self.E:
+            return True
+        return False
+
+    def read_from_file(self):
+        try:
+            i = 0
+            with open('../data/grammar') as f:
+                for line in f.readlines():
+                    if i <= 2:
+                        tokens = line.split(" ")
+                        for j in range(0, len(tokens), 1):
+                            if i == 0:
+                                self.N = self.N + tokens[j]
+                            if i == 1:
+                                self.E = self.E + tokens[j]
+                            if i == 2:
+                                self.S = self.S + tokens[j]
+                    if i > 2:
+                        tokens = line.split(" -> ")
+                        rules = []
+
+                        print(tokens)
+
+                        for rule in tokens[1].split(" | "):
+                            rules.append([rule.split(" ")])
+                        self.P = self.P + str((tokens[0], rules))
+
+                    i = i + 1
+        except IOError as e:
+            print(e)
+
+    def get_productions_for_non_terminal(self, non_terminal):
+        if not self.is_non_terminal(non_terminal):
+            raise Exception('Can only show productions for non-terminals')
+
+        return [prod for prod in self.P if prod[0] == non_terminal]
+
+    def get_productions_containing_non_terminal(self, non_terminal):
+        if not self.is_non_terminal(non_terminal):
+            raise Exception('Can only show productions for non-terminals')
+
+        return [prod for prod in self.P
+                for rule in prod.getRules()
+                if rule[non_terminal] != -1]
+
+    def get_productions(self):
+        return self.P
+
+    def get_non_terminals(self):
+        return self.N
+
+    def get_terminals(self):
+        return self.E
+
+    def get_starting_symbol(self):
+        return self.S
+
+    def __str__(self):
+        return 'N = { ' + ', '.join(self.N) + ' }\n' \
+               + 'E = { ' + ', '.join(self.E) + ' }\n' \
+               + 'P = { ' + ', '.join([' -> '.join(prod) for prod in self.P]) + ' }\n' \
+               + 'S = ' + str(self.S) + '\n'
+print("1.Grammar from the requested file -------------------------------")
+grammar = Grammar("", "", "", "")
+
